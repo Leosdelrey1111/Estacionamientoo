6dc07c7a21e2d6fda0b2dda01c0da6899328e116
Leosdelrey1111 <hernandezleonel20434@gmail.com>
2024-08-25 00:16:00 -0600

    Tu mensaje de commit

--- src/app/entradas/services/supplier-service.service.ts
+++ src/app/entradas/services/supplier-service.service.ts
@@ -9,6 +9,7 @@ import { Supplier } from '../interfaces/supplier'; // Asegúrate de que la ruta 
 export class SupplierService {
   private apiUrl = 'http://localhost:3000/suplier'; // Asegúrate de que esta URL sea correcta
 
+  
   constructor(private http: HttpClient) {}
 
   registerSupplier(supplier: Supplier): Observable<any> {
--- src/app/entradas/services/usuario.service.ts
+++ src/app/entradas/services/usuario.service.ts
@@ -7,7 +7,6 @@ import { Usuario } from '../interfaces/usuario';
   providedIn: 'root'
 })
 export class UserService {
-  
   private apiUrl = 'http://localhost:3000/usuarios';
 
   constructor(private http: HttpClient) {}
